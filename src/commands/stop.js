0a222e56388f900641891af22806fb7fbda76309
Jordan Brown <jbrown725@gmail.com>
2020-12-07 18:28:55 -0800

    fixed stop command

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -3,6 +3,9 @@ module.exports = {
   name: 'stop',
   description: 'stop playing and empty queue',
   execute(message, args) {
+    // get this server's queue out of the client's queues map
+    const serverQueue = message.client.queues.get(message.guild.id);
+
     if (!message.member.voice.channel)
       return message.channel.send(
         "You have to be in a voice channel to stop the music!"
@@ -15,15 +18,3 @@ module.exports = {
     serverQueue.connection.dispatcher.end();
   },
 };
-function stop(message, serverQueue) {
-  if (!message.member.voice.channel)
-    return message.channel.send(
-      "You have to be in a voice channel to stop the music!"
-    );
-  if (!serverQueue || serverQueue.songs.length == 0)
-    return message.channel.send(
-      "Hold your tomatoes, I'm not playing anything!"
-    );
-  serverQueue.songs = [];
-  serverQueue.connection.dispatcher.end();
-}
