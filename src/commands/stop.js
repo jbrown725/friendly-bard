c8814d8428b70984b102d01e9c7e47b90a7e076d
Jordan Brown <jbrown725@gmail.com>
2021-05-26 00:56:52 -0700

    add curly brackets

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -6,14 +6,16 @@ module.exports = {
     // get this server's queue out of the client's queues map
     const serverQueue = message.client.queues.get(message.guild.id);
 
-    if (!message.member.voice.channel)
+    if (!message.member.voice.channel) {
       return message.channel.send(
         "You have to be in a voice channel to stop the music!"
       );
-    if (!serverQueue || serverQueue.songs.length == 0)
+    }
+    if (!serverQueue || serverQueue.songs.length == 0) {
       return message.channel.send(
         "Hold your tomatoes, I'm not playing anything!"
       );
+    }
     serverQueue.songs = [];
     serverQueue.connection.dispatcher.end();
   },
